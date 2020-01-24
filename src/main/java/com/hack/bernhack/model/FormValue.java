12cffede74ded0986ef15da293a317bbf7af47b0
kshitij kafle <kshitijkafle10@gmail.com>
2020-01-24 23:13:40 +0545

    kshitij second commit

---
+++ src/main/java/com/hack/bernhack/model/FormValue.java
@@ -0,0 +1,61 @@
+package com.hack.bernhack.model;
+
+import java.util.Date;
+
+public class FormValue {
+	private String districtName;
+	private String nagarpalika;
+	private String category;
+	private String description;
+	private Date date;
+	private String witness;
+	private String phone;
+	
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public String getNagarpalika() {
+		return nagarpalika;
+	}
+	public void setNagarpalika(String nagarpalika) {
+		this.nagarpalika = nagarpalika;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getWitness() {
+		return witness;
+	}
+	public void setWitness(String witness) {
+		this.witness = witness;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+	
+
+}
